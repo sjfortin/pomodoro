54cd852ed15652549d9fee8aa2b4ad4236b972be
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-21 14:56:54 -0500

    style changes. remove controls, progress on page load. layout updates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 /*
 To Do:
-1. Change input to a select field
-    a. Add a submit button or if the enter key is pressed
-    b. Stop current session if the submit button is clicked during a session
-5. Reorder functions for ease of reading and organization. Emulate the MVC model
 6. Connect to spotify to play music during pomodoro
     a. Ambient during the model.sessionTime
     b. Beats during the breakTime
@@ -21,31 +17,34 @@ var sessionProgress = sessionTime;
 var breakTime = 5 * 60;
 var breakProgress = breakTime;
 var sessionOrBreakText = document.getElementById('sessionOrBreakText');
-sessionOrBreakText.innerHTML = 'Session';
 var sessionOrBreak = 'session';
+var progressBar = document.getElementById('progress');
+progressBar.style.display = 'none';
 
 var selectSessionLength = document.getElementById("selectSessionLength");
 var selectedSessionLength = selectSessionLength.options[selectSessionLength.selectedIndex].value;
 var selectBreakLength = document.getElementById("selectBreakLength");
 var selectedBreakLength = selectBreakLength.options[selectBreakLength.selectedIndex].value;
-var submitTime = document.getElementById('submitTime');
 
 var model = {
     sessionSetTime: 25,
     breakSetTime: 5,
     playStatus: 'stopped',
-  setSessionTime: function(event) {
-    controls.style.display = '';
-    this.sessionSetTime = selectSessionLength.options[selectSessionLength.selectedIndex].value;
-    sessionTime = this.sessionSetTime * 60;
-    sessionProgress = this.sessionSetTime * 60;
-    sessionTimer.textContent = this.sessionSetTime + ' minutes';
-
-    this.breakSetTime = selectBreakLength.options[selectBreakLength.selectedIndex].value;
-    breakTime = this.breakSetTime * 60;
-    breakProgress = this.breakSetTime * 60;
-  },
   startPlaying: function() {
+    progressBar.style.display = '';
+
+    if(this.playStatus === 'stopped') {
+      this.sessionSetTime = selectSessionLength.options[selectSessionLength.selectedIndex].value;
+      sessionTime = this.sessionSetTime * 60;
+      sessionProgress = this.sessionSetTime * 60;
+      sessionTimer.textContent = this.sessionSetTime + ' minutes';
+
+      this.breakSetTime = selectBreakLength.options[selectBreakLength.selectedIndex].value;
+      breakTime = this.breakSetTime * 60;
+      breakProgress = this.breakSetTime * 60;
+      sessionOrBreak = 'session';
+    }
+
     if(this.playStatus !== 'playing') {
       this.playStatus = 'playing';
       if(sessionOrBreak === 'session') {
@@ -74,6 +73,9 @@ var model = {
         }
         sessionOrBreakText.innerHTML = 'Session';
         document.querySelector('.progress').value = (sessionProgress / sessionTime) * 100;
+        progressBar.classList.remove('is-warning');
+        progressBar.classList.add('is-info');
+        play.classList.add('active-play-session');
         sessionProgress--;
         window.setTimeout(sessionCountdown, 100);
       }
@@ -98,27 +100,38 @@ var model = {
         }
         sessionOrBreakText.innerHTML = 'Break';
         document.querySelector('.progress').value = (breakProgress / breakTime) * 100;
+        progressBar.classList.remove('is-info');
+        progressBar.classList.add('is-warning');
+        play.classList.add('active-play-break');
         breakProgress--;
         window.setTimeout(breakCountdown, 100);
       }
     }
   },
   stopPlaying: function() {
-    sessionOrBreak = 'session';
+    progressBar.style.display = 'none';
+    play.classList.remove('active-play-session');
+    play.classList.remove('active-play-break');
+    sessionOrBreakText.innerHTML = '';
+    progressBar.classList.remove('is-warning');
+    progressBar.classList.remove('is-info');
     sessionProgress = this.sessionSetTime * 60;
     breakProgress = this.breakSetTime * 60;
     this.playStatus = 'stopped';
-    sessionTimer.textContent = this.sessionSetTime + ' minutes';
+    sessionTimer.innerHTML = '';
+    document.querySelector('.progress').value = 100;
   },
   pausePlaying: function() {
+    play.classList.remove('active-play-session');
+    play.classList.remove('active-play-break');
+    progressBar.classList.remove('is-warning');
+    progressBar.classList.remove('is-info');
     this.playStatus = 'paused';
   }
 };
 
 var view = {
   setUpEventListeners: function() {
-    controls.style.display = 'none';
-    submitTime.addEventListener('click', model.setSessionTime.bind(model));
     play.addEventListener('click', model.startPlaying.bind(model));
     stop.addEventListener('click', model.stopPlaying.bind(model));
     pause.addEventListener('click', model.pausePlaying.bind(model));
